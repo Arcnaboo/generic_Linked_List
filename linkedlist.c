207f6db431ebf7ab6c72c81d5374a6d650089449
Arda Akgur <aakgur@gmail.com>
2025-06-14 23:42:01 +0300

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -1,10 +1,14 @@
-// linkedlist.c
 #include <stdio.h>
 #include <stdlib.h>
 #include "linkedlist.h"
 
 void add_node(LinkedList* list, void* data) {
     Node* new_node = (Node*) malloc(sizeof(Node));
+    if (!new_node) {
+        fprintf(stderr, "Failed to allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+
     new_node->data = data;
     new_node->next = list->head;
     list->head = new_node;
@@ -13,15 +17,16 @@ void add_node(LinkedList* list, void* data) {
 
 void remove_first_node(LinkedList* list) {
     if (!list->head) return;
+
     Node* temp = list->head;
-    list->head = list->head->next;
+    list->head = temp->next;
     free(temp);
     list->size--;
 }
 
 void clear_list(LinkedList* list) {
     while (list->head) {
-        remove_first_node(list);
+        list->remove_first(list);  // Call via function pointer
     }
 }
 
@@ -36,6 +41,8 @@ void print_list(LinkedList* list, void (*print_func)(void*)) {
 void init_list(LinkedList* list) {
     list->head = NULL;
     list->size = 0;
+
+    // Hooking function pointers (aka 'methods')
     list->add = add_node;
     list->remove_first = remove_first_node;
     list->clear = clear_list;
